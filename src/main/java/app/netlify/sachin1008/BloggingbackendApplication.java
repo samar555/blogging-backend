ec3e95ea7eb93b4cf08d34b0cb90fb97da9aeb51
samar555 <sachinmishraravi@gmail.com>
2025-04-30 19:08:06 +0530

    lets rock this project

---
+++ src/main/java/app/netlify/sachin1008/BloggingbackendApplication.java
@@ -0,0 +1,13 @@
+package app.netlify.sachin1008;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggingbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloggingbackendApplication.class, args);
+	}
+
+}
